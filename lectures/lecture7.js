9457ec985535f2737cb8c832bca941a2ce472968
Max-Andre' Zils <zilsmc@gmail.com>
2018-03-27 08:00:02 +0200

    Added headings to all the lecture notes

---
+++ lectures/lecture7.js
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Lecture 7: Statements and Expressions                                                          //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Statement: Performs an action
+function someFun(par) {
+  //code
+}
+
+// Function Expression: Produces a Value
+let someFun = function (par) {
+  //code
+}
+// Expressions:
+3 + 4;
+var x = 3;
+
+// Statements:
+if(x === 3){
+  //do something
+}
