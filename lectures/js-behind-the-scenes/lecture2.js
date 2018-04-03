0cbc181f84dc2147e5f1d572067bae9fd3de56db
Max-Andre' Zils <zilsmc@gmail.com>
2018-04-03 17:00:37 +0200

    completed lecture 2 of behind the scenes

--- lectures/js-behind-the-scenes/lecture2.js
+++ lectures/js-behind-the-scenes/lecture2.js
@@ -1,10 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+// Lecture 2: Hoising Explained                                               //
+////////////////////////////////////////////////////////////////////////////////
 /**
- * calculateAge - Description
+ * calculateAge - Accepts the year of birth as an argument and returns the age.
  *
- * @param {int} year of birth
+ * @param {int} year  year of birth
  *
  * @returns {int} age
  */
 function calculateAge(year) {
   return parseInt(new Date().getFullYear() - year);
 }
+
+/**
+ * retirement - Description
+ *
+ * @param {int} year enter the year of birth as a argument
+ *
+ * @returns {int} the years until retirment
+ */
+let retirement = function (year){
+  return (65 - parseInt(new Date().getFullYear() - year));
+}
+console.log(retirement(1999));
