fcb77c37e2b0492b0ce15bdf08f8bbbbed4f1355
Max-Andre' Zils <zilsmc@gmail.com>
2018-04-03 16:48:43 +0200

    converted eslint to json

--- lectures/js-behind-the-scenes/lecture2.js
+++ lectures/js-behind-the-scenes/lecture2.js
@@ -1,11 +1,10 @@
-// Lecture 2:Hoisting
-
 /**
-* calculateAge calculates the age based on year of yearsOfBirth
-* @param {int} year year of birth
-* @returns {int} age.
-*/
-
+ * calculateAge - Description
+ *
+ * @param {int} year of birth
+ *
+ * @returns {int} age
+ */
 function calculateAge(year) {
   return parseInt(new Date().getFullYear() - year);
 }
