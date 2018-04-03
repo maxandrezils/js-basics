8a3f1b5c7a7bfb6b3b5e029ea1ee5c01336e8724
Max-Andre' Zils <zilsmc@gmail.com>
2018-04-03 13:57:45 +0200

    added editorconfig

--- lectures/js-behind-the-scenes/lecture2.js
+++ lectures/js-behind-the-scenes/lecture2.js
@@ -1,1 +1,11 @@
 // Lecture 2:Hoisting
+
+/**
+* calculateAge calculates the age based on year of yearsOfBirth
+* @param {int} year year of birth
+* @returns {int} age.
+*/
+
+function calculateAge(year) {
+  return parseInt(new Date().getFullYear() - year);
+}
