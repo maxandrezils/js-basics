bea3597a674fb8f9c886563817852ca4ee290ad7
Max-Andre' Zils <zilsmc@gmail.com>
2018-04-06 14:53:54 +0200

    dom manipulation project
    continued work on pig game

--- lectures/js-dom-manipulation/app.js
+++ lectures/js-dom-manipulation/app.js
@@ -1,13 +1,51 @@
 /*
 GAME RULES:
-
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 let scores = [0, 0];
 let roundScore = 0;
 let activePlayer = 0;
+
+function rollDice(){
+  let dice = Math.floor(Math.random() * 6) + 1;
+  document.querySelector('.dice').src = 'dice-' + dice + '.png';
+  updateCurrentScore(dice);
+}
+function updateCurrentScore(diceScore){
+  if(diceScore != 1){
+    roundScore += diceScore;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }else{
+    roundScore = 0;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    changeActivePlayer();
+  }
+}
+
+function hold(){
+  changeActivePlayer();
+  activePlayerstyling();
+}
+
+function changeActivePlayer(){
+  if(activePlayer == 0){
+    activePlayer = 1;
+  }else{
+    activePlayer = 0;
+  }
+}
+function activePlayerstyling(){
+  let sectionStyling0 = document.querySelector('.player-0-panel').classList;
+  let sectionStyling1 = document.querySelector('.player-1-panel').classList;
+  sectionStyling0.toggle('active');
+  sectionStyling1.toggle('active');
+}
+
+function updateTotalScores(){
+  
+}
