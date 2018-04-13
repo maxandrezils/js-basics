6995783ed258898698844e2941fcb053794c2dc2
Max-Andre' Zils <zilsmc@gmail.com>
2018-04-13 07:53:33 +0200

    started on challenges

--- lectures/js-dom-manipulation/app.js
+++ lectures/js-dom-manipulation/app.js
@@ -1,11 +1,15 @@
 /*
 GAME RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+  1. The game has 2 players, playing in rounds
+  2. In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+  2.1. BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+  3. The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+  4. The first player to reach 100 points on GLOBAL score wins the game
 
+  Challenges:
+  1. A player loses his/her entire score when rolling two sixes in a row.
+  2. Add an input field to the HTML where players can set the winning score so that they can change the predifined score of 100
+  3. Add another dice to the game so that there are two dice. The player loses their current score if one of the die is 1.
 */
 let scores = [0, 0];
 let roundScore = 0;
@@ -44,6 +48,7 @@ function activePlayerstyling(){
   sectionStyling1.toggle('active');
 }
 
+<<<<<<< refs/remotes/origin/master
 function addRoundToTotalScore(){
   scores[activePlayer] += roundScore;
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
@@ -69,4 +74,8 @@ function hold(){
   activePlayerstyling()
   changeActivePlayer()
   winCondition();
+=======
+function updateTotalScores(){
+
+>>>>>>> started on challenges
 }
