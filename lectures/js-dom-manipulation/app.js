b36f2ce33ec8cf0a763516f74641f8ecb70a289e
Max-Andre' Zils <zilsmc@gmail.com>
2018-05-18 21:59:33 +0200

    started on the reset functionality

--- lectures/js-dom-manipulation/app.js
+++ lectures/js-dom-manipulation/app.js
@@ -1,21 +1,24 @@
 /*
 GAME RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+  - The game has 2 players, playing in rounds
+  - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+  - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+  - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+  - The first player to reach 100 points on GLOBAL score wins the game
 
-  Challenges:
-Change the game to follow these rules:
-
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+Challenges:
+  Change the game to follow these rules:
+  1. A player looses his ENTIRE score when he rolls two 6 in a row. After that,
+     it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+  2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100.
+     (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+  3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1.
+     (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 let scores = [0, 0];
 let roundScore = 0;
 let activePlayer = 0;
+let winScore = 100;
 
 const changeActivePlayer = () => {
   activePlayer = activePlayer == 0 ? 1 : 0;
@@ -27,7 +30,7 @@ const activePlayerstyling = () => {
   let sectionStyling1 = document.querySelector('.player-1-panel').classList;
   sectionStyling0.toggle('active');
   sectionStyling1.toggle('active');
-};
+  };
 
 const rollDice = document.querySelector('.btn-roll').
   addEventListener('click', () => {
@@ -61,7 +64,7 @@ const addRoundToTotalScore = () => {
 };
 
 const winCondition = () => {
-  if(scores[activePlayer] >= 100) alert(`Player ${activePlayer + 1} has won`);
+  if(scores[activePlayer] >= winScore) alert(`Player ${activePlayer + 1} has won`);
   // TODO: add logic to update styling for winner
 };
 
@@ -79,7 +82,10 @@ const reset = document.querySelector('.btn-new').
     }
   }
 );
-
+const setPlayerOneActive = () => {
+  // TODO add class for active to player 1 if it is not active
+  // TODO remove class for active if player 2 is active and apply it to player 1
+};
 const hold = document.querySelector('.btn-hold').
   addEventListener('click', () => {
     addRoundToTotalScore();
