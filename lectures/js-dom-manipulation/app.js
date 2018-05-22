3373a99f3827f46b8233804206c95aed44f55896
Max-Andre' Zils <zilsmc@gmail.com>
2018-05-22 19:19:48 +0200

    added jshint to the repo

--- lectures/js-dom-manipulation/app.js
+++ lectures/js-dom-manipulation/app.js
@@ -1,19 +1,44 @@
 /*
+--------------------------------------------------------------------------------
 GAME RULES:
+--------------------------------------------------------------------------------
   - The game has 2 players, playing in rounds
-  - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-  - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-  - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+  - In each turn, a player rolls a dice as many times as he whishes.
+    Each result get added to his ROUND score
+  - BUT, if the player rolls a 1, all his ROUND score gets lost. After that,
+    it's the next player's turn
+  - The player can choose to 'Hold', which means that his ROUND score gets
+  added to his GLOBAL score. After that, it's the next player's turn
   - The first player to reach 100 points on GLOBAL score wins the game
-
-Challenges:
+--------------------------------------------------------------------------------
+CHALLENGES:
+--------------------------------------------------------------------------------
   Change the game to follow these rules:
   1. A player looses his ENTIRE score when he rolls two 6 in a row. After that,
-     it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-  2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100.
-     (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-  3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1.
-     (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+     it's the next player's turn. (Hint: Always save the previous dice roll in
+     a separate variable)
+  2. Add an input field to the HTML where players can set the winning score, so
+     that they can change the predefined score of 100.
+     (Hint: you can read that value with the .value property in JavaScript.
+     This is a good oportunity to use google to figure this out :)
+  3. Add another dice to the game, so that there are two dices now. The player
+     looses his current score when one of them is a 1.
+     (Hint: you will need CSS to position the second dice, so take a look at
+     the CSS code for the first one.)
+--------------------------------------------------------------------------------
+DECLARATIONS:
+--------------------------------------------------------------------------------
+  1. scores       -- score for both activePlayerStyling
+  2. roundScore   -- the current score being updated by each rolls
+  3. activePlayer -- current player rolling the dice
+  4. winScore     -- the win winCondition
+--------------------------------------------------------------------------------
+FUNCTIONS:
+--------------------------------------------------------------------------------
+  1. changeActivePlayer()  -- changes the current active player
+  2. activePlayerStyling() -- toggles the styling for the active player
+  3.
+--------------------------------------------------------------------------------
 */
 let scores = [0, 0];
 let roundScore = 0;
@@ -23,9 +48,10 @@ let winScore = 100;
 const changeActivePlayer = () => {
   activePlayer = activePlayer == 0 ? 1 : 0;
   resetRoundScore();
+  activePlayerStyling();
 };
 
-const activePlayerstyling = () => {
+const activePlayerStyling = () => {
   let sectionStyling0 = document.querySelector('.player-0-panel').classList;
   let sectionStyling1 = document.querySelector('.player-1-panel').classList;
   sectionStyling0.toggle('active');
@@ -45,7 +71,6 @@ const rollDice = document.querySelector('.btn-roll').
     document.querySelector(`#current-${activePlayer}`).textContent = roundScore.toString();
   }else{
     changeActivePlayer();
-    activePlayerstyling();
   }
 });
 
@@ -79,18 +104,22 @@ const reset = document.querySelector('.btn-new').
       document.querySelector('#current-0').textContent = '0';
       document.querySelector('#current-0').textContent = '0';
       // TODO: Set player one as active player
+      setPlayerOneActive();
     }
   }
 );
 const setPlayerOneActive = () => {
-  // TODO add class for active to player 1 if it is not active
-  // TODO remove class for active if player 2 is active and apply it to player 1
+  if(activePlayer === 0 ){
+    // TODO add class for active to player 1 if it is not active
+  }else{
+    // TODO remove class for active if player 2 is active and apply it to player 1
+  }
 };
+
 const hold = document.querySelector('.btn-hold').
   addEventListener('click', () => {
-    addRoundToTotalScore();
-    activePlayerstyling()
-    changeActivePlayer()
     winCondition();
+    addRoundToTotalScore();
+    changeActivePlayer();
   }
 );
